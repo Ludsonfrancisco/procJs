a58939233cd60b1f3ca13c616a224f334493e030
Ludson Francisco <ludsonfrancisco1010@gmail.com>
2021-02-10 15:39:42 -0300

    altered index js

--- public/excel.js
+++ public/excel.js
@@ -5,7 +5,7 @@ document.getElementById('input').addEventListener('change', (event) => {
 })
 
 
-const selecionado = document.getElementById('button').addEventListener('click', () => {
+document.getElementById('button').addEventListener('click', () => {
     if(selectedFile){
         let fileReader = new FileReader()
         fileReader.readAsBinaryString(selectedFile)
@@ -14,13 +14,28 @@ const selecionado = document.getElementById('button').addEventListener('click', 
             let workbook = XLSX.read(data, {type:'binary'})
             // console.log(workbook)
             workbook.SheetNames.forEach(sheet => {
-            let rowObject = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheet])
+            let rowObject = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheet])    
             
+            const retirarVazio = item => item !== ''
+            const pon = item => item.PON   
+            const recurso = item => item.Recurso
+            const cidade = item => item.Cidade
+            const ard = item => item['ID de Armário']       
+            
+            const create = (pon) => {
+                return pon
+            }
+
+            const getPon = rowObject.map(pon).filter(retirarVazio)
+            const getCidade = rowObject.map(cidade).filter(retirarVazio)
+            const getRecurso = rowObject.map(recurso).filter(retirarVazio)
+
+            // console.log(create(getPon))
+
             document.getElementById('jsondata').innerHTML = JSON.stringify(rowObject, undefined, 4)
           
             });
         }
     }    
 })
-
 
